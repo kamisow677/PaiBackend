d7525f798507d8f14087c780af80c22b90ea84a5
jacekan <>
2019-12-17 19:20:03 +0100

    add swagger, add requirements.txt

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from django.urls import path
-from rest_framework.renderers import CoreJSONRenderer
-from rest_framework.schemas import get_schema_view
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
-from . import views
-from api.views.location_view import LocationList, LocationDetail
-from api.views.user_view import LoginView, LogoutView
 from api.views.location_user_view import LocationUserList, LocationUserDetailList
+from api.views.location_view import LocationList, LocationDetail
 from api.views.user_register_view import LocationUserRegister
+from api.views.user_view import LoginView, LogoutView
 
 # urlpatterns = [
 #     url(
@@ -22,18 +22,25 @@ from api.views.user_register_view import LocationUserRegister
 #     )
 # ]
 
-from rest_framework.schemas import get_schema_view
-from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
 
-schema_view = get_schema_view(title='Users API', renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
+schema_view = get_schema_view(
+    openapi.Info(
+        title="PAI Backend API",
+        default_version='v1'
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('locations/', LocationList.as_view(), name="locations_bezpk"),
-    path('locations/<int:pk>/', LocationDetail.as_view(), name = "locations_zpk"),
+    path('locations/<int:pk>/', LocationDetail.as_view(), name="locations_zpk"),
     path('user/locations/', LocationUserList.as_view(), name="user_locations_bezpk"),
     path('user/locations/<int:pk>/', LocationUserDetailList.as_view(), name="user_locations_zpk"),
     path('user/register', LocationUserRegister.as_view(), name="register"),
     path('login/', LoginView.as_view(), name="login"),
-    path('pla/', schema_view, name="docs"),
     path('logout/', LogoutView.as_view(), name="logout"),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
